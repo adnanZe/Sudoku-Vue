ef803a57509b6ba65fd0f06c5d64e9215c5c24d6
AdnanZe <adnan.zecheria@yahoo.com>
2023-01-30 20:14:56 +0200

    [small] update

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app7 = new Vue({
       { id: 0, text: "Vegetables" },
       { id: 1, text: "Cheese" },
       { id: 2, text: "Whatever else humans are supposed to eat" },
+      { id: 3, text: "Fruits" },
     ],
   },
 });
