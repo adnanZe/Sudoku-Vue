146ca30f1c0b36fc9095d85416c2f33f5f9da6bc
AdnanZe <adnan.zecheria@yahoo.com>
2023-02-06 16:03:14 +0200

    [setup] added vuejs with cli

---
+++ vuejs-sudoku/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'fist vue file!'
+    }
+  },
+
+  methods: {
+    greeting: function() {
+      return 'heey'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
